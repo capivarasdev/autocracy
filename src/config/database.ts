201c8e26ad2b787ac3a89f94705bcc47a73df741
codando <joao_pereira@gec.inatel.br>
2023-02-26 18:03:44 -0300

    feat: add postgres

--- src/config/database.ts
+++ src/config/database.ts
@@ -12,7 +12,7 @@ export const databaseConfig: DatabaseConfigType = {
     }
 }
 
-export const databaseType = 'better-sqlite' as const // 'better-sqlite' | 'sqlite' | 'postgres' | 'mysql' | 'mariadb' | 'mongo'
+export const databaseType = 'postgresql' as const // 'better-sqlite' | 'sqlite' | 'postgres' | 'mysql' | 'mariadb' | 'mongo'
 
 const envMikroORMConfig: { production: Options, development?: Options } = {
 
@@ -21,8 +21,8 @@ const envMikroORMConfig: { production: Options, development?: Options } = {
         /**
          * SQLite
          */
-        type: databaseType,
-        dbName: `${databaseConfig.path}db.sqlite`,
+        // type: databaseType,
+        // dbName: `${databaseConfig.path}db.sqlite`,
 
         /**
          * MongoDB
@@ -33,12 +33,12 @@ const envMikroORMConfig: { production: Options, development?: Options } = {
         /**
          * PostgreSQL
          */
-        // type: databaseType,
-        // dbName: process.env['DATABASE_NAME'],
-        // host: process.env['DATABASE_HOST'],
-        // port: Number(process.env['DATABASE_PORT']),,
-        // user: process.env['DATABASE_USER'],
-        // password: process.env['DATABASE_PASSWORD'],
+        type: databaseType,
+        dbName: process.env['DATABASE_NAME'],
+        host: process.env['DATABASE_HOST'],
+        port: Number(process.env['DATABASE_PORT']),
+        user: process.env['DATABASE_USER'],
+        password: process.env['DATABASE_PASSWORD'],
 
         /**
          * MySQL
