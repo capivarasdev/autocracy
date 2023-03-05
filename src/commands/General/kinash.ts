e7dee54ce74df2307020e322d65edb8db454b2ef
Júnio Teixeira <junio-82@hotmail.com>
2023-03-04 23:27:46 -0300

    fix(commands): duplicated slash commands

--- src/commands/General/kinash.ts
+++ src/commands/General/kinash.ts
@@ -1,9 +1,11 @@
 import { Category } from "@discordx/utilities";
 import type { CommandInteraction, Message } from "discord.js";
+import { Guild } from "discordx";
 
 import { Discord, Slash } from "@decorators";
 
 @Discord()
+@Guild('929170974262313021')
 @Category('General')
 export default class KinashCommand {
     @Slash({
